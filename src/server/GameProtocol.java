c0fb2c156bce10ef4a9b106c7adad8393ba49404
yadonx <ej_yo@hotmail.com>
2020-11-20 16:53:55 +0100

    added properties

--- src/server/GameProtocol.java
+++ src/server/GameProtocol.java
@@ -3,7 +3,11 @@ package server;
 import model.Category;
 import model.QuestionDatabase;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * Created by Filip Jakobsson
@@ -16,16 +20,32 @@ public class GameProtocol {
 
     QuestionDatabase questionDatabase;
     private int currentCategory = 0;
+    private int rounds;
+    private int questions;
 
     List<Category> categoryList;
 
-    GameProtocol(){
+    GameProtocol() {
+        Properties roundProperties = new Properties();
+        Properties questionProperties = new Properties();
+        try {
+            roundProperties.load(new FileInputStream("src/server/properties/threeRound.properties"));
+            questionProperties.load(new FileInputStream("src/server/properties/threeQuestion.properties"));
+        }catch (IOException  e){
+            e.printStackTrace();
+        }
+        rounds = Integer.parseInt(roundProperties.getProperty("round"));
+        questions = Integer.parseInt(questionProperties.getProperty("questions"));
+
+        System.out.println(rounds);
+
         questionDatabase = new QuestionDatabase();
-        categoryList = questionDatabase.getShuffledCategories(3);//Todo fixa med Properties.
+        categoryList = questionDatabase.getShuffledCategories(rounds);//Todo fixa med Properties.
     }
 
     public Category getCategory(){
         Category temp = categoryList.get(currentCategory);
+        temp.getShuffledQuestions(questions);
         currentCategory++;
         return temp;
     }
